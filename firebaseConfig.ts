3d866c2050a9be311f0b5aa1df21e23e530e3629
React Mastering <pga.adam.scott@gmail.com>
2022-03-30 23:39:01 +0900

    fixed config file

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,15 +1,15 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp, getApps, getApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-
-const firebaseApp = initializeApp({
+const config = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-})
+}
 
-export const auth = getAuth(firebaseApp)
-export const db = getFirestore(firebaseApp)
+const firebaseApp = !getApps().length ? initializeApp(config) : getApp()
+export const auth = getAuth()
+export const db = getFirestore()
